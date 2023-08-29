6ee59b1de135dce248c00e87d31c2520427068a0
TemaTkol <tematkol@gmail.com>
2023-08-29 23:23:46 +0300

    Init

---
+++ ChainDoku/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChainDoku
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
