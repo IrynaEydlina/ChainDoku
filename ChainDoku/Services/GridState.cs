82d1c71ca3a0126b6a2b6c9b2a2745bfac2090e9
TemaTkol <tematkol@gmail.com>
2023-09-06 23:05:34 +0300

    Ui improvements
    Moved cells to razor components
    Improved performance
    Add KeepScreenOn set

---
+++ ChainDoku/Services/GridState.cs
@@ -0,0 +1,8 @@
+﻿namespace ChainDoku.Services;
+
+public class GridState
+{
+    public int? ActiveRow { get; set; }
+    public int? ActiveColumn { get; set; }
+    public int? ActiveValue { get; set; }
+}
